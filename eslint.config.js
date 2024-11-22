c41f0365e15498bc3395af1d12cf8b19b9543823
Corentin <godartcn@gmail.com>
2024-11-22 01:33:04 +0100

    build: setting up the library

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import typescriptParser from '@typescript-eslint/parser';
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist', 'coverage'] },
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: typescriptParser,
+    },
+    plugins: {
+      'prettier': prettier,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...prettier.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          'react': 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+});
